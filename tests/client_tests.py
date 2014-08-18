64fc1088cdbfc433ae14484275a52a89db030d70
Sophia Davis <scdgrapefruit@gmail.com>
2014-08-18 14:00:23 -0400

    Clean up unnecessary comments

--- tests/client_tests.py
+++ tests/client_tests.py
@@ -44,35 +44,9 @@ class ClientTests(unittest.TestCase):
         self.assertEqual(status, 0)
         self.assertEqual(self.client.connection_id, new_connection_id)
         
-#     def test_announce_packet_contains_current_connection_id(self):
-#         
-#     def make_announce_packet(self):
-# '''   
-# size	        name	        description
-# int64_t	        connection_id	The connection id acquired from establishing the connection.
-# int32_t	        action	        Action. in this case, 1 for announce. See actions.
-# int32_t	        transaction_id	Randomized by client.
-# int8_t[20]	    info_hash	    The info-hash of the torrent you want announce yourself in.
-# int8_t[20]	    peer_id	        Your peer id.
-# int64_t	        downloaded	    The number of byte you've downloaded in this session.
-# int64_t	        left	        The number of bytes you have left to download until you're finished.
-# int64_t	        uploaded	    The number of bytes you have uploaded in this session.
-# int32_t	        event           The event, one of:
-#                                     none = 0
-#                                     completed = 1
-#                                     started = 2
-#                                     stopped = 3
-#                         
-# uint32_t	    ip	            Your ip address. Set to 0 if you want the tracker to use the sender of this udp packet.
-# uint32_t	    key	            A unique key that is randomized by the client.
-# int32_t	        num_want	    The maximum number of peers you want in the reply. Use -1 for default.
-# uint16_t	    port	        The port you're listening on.
-# uint16_t	    extensions	    See extensions
-# ''' 
         
-    
-    # to test:
-    # send_packet_to_tracker(self, sock, host, port, packet)
+        
+#     def # send_packet_to_tracker(self, sock, host, port, packet):
     
 
 if __name__ == '__main__':
